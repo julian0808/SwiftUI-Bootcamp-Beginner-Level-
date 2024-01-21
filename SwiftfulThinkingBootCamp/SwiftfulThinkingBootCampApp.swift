bc259a70f329ec86da96959ec9a5187c434aca07
julian0808 <julianm08@gmail.com>
2024-01-21 15:43:34 -0500

    Initial Commit

---
+++ SwiftfulThinkingBootCamp/SwiftfulThinkingBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootCampApp.swift
+//  SwiftfulThinkingBootCamp
+//
+//  Created by Julian Mesa on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
